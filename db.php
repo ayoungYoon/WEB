bb2a453f4f644d46b550e359e53a864ed89d8f1e
ayoungYoon <166527083+ayoungYoon@users.noreply.github.com>
2024-04-30 00:28:03 +0900

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,15 @@
 <?php
-$host = 'localhost'; // MySQL 호스트 주소
-$dbname = 'your_database_name'; // 데이터베이스 이름
-$username = 'your_username'; // MySQL 사용자 이름
-$password = 'your_password'; // MySQL 비밀번호
+$host = 'localhost'; 
+$dbname = 'your_database_name'; 
+$username = 'your_username'; 
+$password = 'your_password'; 
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // 연결 성공 시 처리할 내용
+    
 } catch(PDOException $e) {
-    // 연결 실패 시 처리할 내용
+    
     echo "Connection failed: " . $e->getMessage();
 }
 ?>
